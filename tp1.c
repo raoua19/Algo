464967e721f82fd3b1372f25cd1ed0897ffa79d9
raoua19 <raouaamamra2006@gmail.com>
2025-10-21 10:46:08 -1100

    Update tp1.c

--- tp1.c
+++ tp1.c
@@ -1,36 +1,43 @@
 #include <stdio.h>
+
 int add(int a, int b) {
     return a + b;
 }
+
 int max(int a, int b) {
     if (a > b)
         return a;
     else
         return b;
 }
+
 float average(int arr[], int n) {
     int sum = 0;
     for (int i = 0; i < n; i++) {
-        sum = sum + arr[i];
+        sum = add(sum, arr[i]);
     }
-    return (float)sum / n
+    return (float)sum / n;
 }
+
 int main() {
     int n;
     printf("Enter the number of elements: ");
     scanf("%d", &n);
 
-    int arr[100];
-    printf("Enter %d numbers:\n", n);
+    int arr[n];
+    printf("Enter %d integers:\n", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
-    }max()
-    int largest = arr[0];
+    }
+
+    int maxValue = arr[0];
     for (int i = 1; i < n; i++) {
-        largest = max(largest, arr[i]);
-    } average()
+        maxValue = max(maxValue, arr[i]);
+    }
+
     float avg = average(arr, n);
-    printf("\nThe largest number is: %d\n", largest);
+
+    printf("The largest number is: %d\n", maxValue);
     printf("The average is: %.2f\n", avg);
-return 0;
-}
+
+    return 0;
