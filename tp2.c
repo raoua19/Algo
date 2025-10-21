a4f7042133820f3d97c81db0b287a1da8cd86619
raoua19 <raouaamamra2006@gmail.com>
2025-10-21 11:06:19 -1100

    Update tp2.c

--- tp2.c
+++ tp2.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
 
 int main() {
-    int n = 4;
-    int A[4][4] = {
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {9, 10, 11, 12},
-        {13, 14, 15, 16}
-    };
-    int i, j, temp;
+    int n;
+    printf("Enter the size of the matrix: ");
+    scanf("%d", &n);
 
-   
-    for (i = 0; i < n; i++) {
-        for (j = i + 1; j < n; j++) {
-            temp = A[i][j];
+    int A[n][n];
+
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    // Swap upper and lower triangles
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int temp = A[i][j];
             A[i][j] = A[j][i];
             A[j][i] = temp;
         }
     }
 
-   
-    printf("Resulting Matrix:\n");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            printf("%3d ", A[i][j]);
+    printf("\nMatrix after symmetry:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", A[i][j]);
         }
         printf("\n");
     }
 
-    return 0;  }
+    return 0;
+}
